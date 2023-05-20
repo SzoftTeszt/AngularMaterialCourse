0101976e76643b9cac587f09781310dbf0f8f827
JagerAttila <kitalalt@gmail.com>
2023-05-20 10:35:55 +0200

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BasicComponent } from './basic/basic.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+import { FilterComponent } from './filter/filter.component';
+import { EditableComponent } from './editable/editable.component';
+import { AnimalEditorComponent } from './animal-editor/animal-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'basic', component:BasicComponent},
+  {path:'paginator', component:PaginatorComponent},
+  {path:'filter', component:FilterComponent},
+  {path:'editable', component:EditableComponent},
+  {path:'editable/edit/:id', component:AnimalEditorComponent},
+  {path:'**', redirectTo:''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
